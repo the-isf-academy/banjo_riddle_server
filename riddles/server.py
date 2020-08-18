72cb7775f2e155baa6f7da33059010816597767b
Chris Proctor <chris.proctor@gmail.com>
2020-08-18 19:01:17 -0400

    Merged helpers into server

--- riddles/server.py
+++ riddles/server.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def list_riddles():
+    "Returns a list of all the riddles, without answers, and in order of difficulty."
     return {'riddles': [riddle.as_dict(with_answer=False) for riddle in Riddle.all()]}
 
 @app.route('/', methods=['POST'])
 def create_riddle():
+    "Creates a new riddle and returns it. Requires `question` and `answer` params."
     data = request.get_json()
     errors = check_input(data, ["question", "answer"])
     if len(errors) > 0:
@@ -24,6 +26,7 @@ def create_riddle():
 
 @app.route('/<int:id>', methods=['GET'])
 def show_riddle(id):
+    "Returns one riddle, without its answer."
     try:
         riddle = Riddle.get(id)
         return riddle.as_dict(with_answer=False)
@@ -32,6 +35,7 @@ def show_riddle(id):
 
 @app.route('/<int:id>', methods=['POST'])
 def guess_answer(id):
+    "Accepts a `guess` param and returns whether or not it was correct."
     try:
         riddle = Riddle.get(id)
     except Riddle.DoesNotExist:
@@ -47,3 +51,19 @@ def guess_answer(id):
         "correct": correct,
         "riddle": riddle.as_dict(with_answer=correct)
     }
+
+def check_input(values_dict, expected):
+    """
+    A helper to ensure that the data sent with a POST request
+    contains all expected params and no unexpected params. 
+    """
+    errors = []
+    if values_dict is None:
+        return ["no data provided"]
+    for key in values_dict.keys():
+        if key not in expected:
+            errors.append("unexpected field: {}".format(key))
+    for key in expected:
+        if key not in values_dict.keys():
+            errors.append("missing field: {}".format(key))
+    return errors
